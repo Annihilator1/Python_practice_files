790ae935bbc2922608095c7d30c50b44bf91e9b6
Arpit <arpit.22236@gmail.com>
2025-10-06 20:12:35 +0530

    git commit

---
+++ variables.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  4 00:37:01 2025
+
+@author: arpit
+"""
+"""
+x=5
+y=12
+z=x+y
+print(z)  """
+
+
+# phrase_1 = 'The cat sat on the mat!'
+# phrase_2 = 'And so did the dog!'
+# phrase_3 = phrase_1 +' ' + phrase_2
+# print(phrase_3)
+
+# user_input = int(input('How many apples do you have?\n >>>'))
+# print(user_input)
+
+# Step 1: Creating string variables
+greeting = "Hello"
+name = "Alice"
+
+# Step 2: Concatenate strings with a space
+full_greeting = greeting + " " + name
+
+# Step 3: Print the full greeting
+print (full_greeting)
+
+# Step 4: Create a repeated string
+repeat_phrase = "Goodbye "*3
+
+# Step 5: Print the repeated phrase
+print(repeat_phrase)
