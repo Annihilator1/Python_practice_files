7f428939666c79902d63488ea1233055905629d7
Arpit <arpit.22236@gmail.com>
2025-10-06 20:10:18 +0530

    First commit: added initial hello world script

---
+++ print.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  4 00:15:26 2025
+
+@author: arpit
+"""
+
+print('how\'s are you today?')
+print("one")
+print('two')
+print('three')
+print('four')
+
+print("one", end=' ')
+print('two', end=' ')
+print('three', end=' ')
+print('four', end=' ')
+
+print("One, 'two', \"three\", four, \n\\five\\ once, 'I' caught a \n\tfish '//alive\\'")
